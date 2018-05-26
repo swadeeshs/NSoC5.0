04c91bcb15f483999a4bebf8aefa9630f4678430
chawat <chawatyash@gmail.com>
2018-05-26 18:58:20 +0530

    Face Recognition Added | SD Card Updated

---
+++ Face/Face.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+//#include<WiFiClient.h>
+#include "mainPage.h">
+#include<WebServer.h>
+
+char *ssid =  "#1@"; 
+char *pass =  "unxh4995";
+
+WebServer server(80);
+
+void handleVal() {
+  String s = mainpage;
+  server.send(200, "text/html", s);
+}
+
+ 
+void setup() {
+       Serial.begin(9600);
+       delay(10);
+               
+       Serial.print("WiFi Connecting to ");
+       Serial.println(ssid);
+ 
+       WiFi.begin(ssid, pass); 
+       while(WiFi.status() != WL_CONNECTED) {delay(200);}
+       Serial.println("");
+       Serial.print("WiFi connected at ");
+       Serial.println(WiFi.localIP()); 
+
+       server.on("/", handleVal);
+       server.begin();
+       delay(20);
+       Serial.println("Server has Started");
+}
+ 
+void loop() {      
+  server.handleClient();
+}
+
+
+
---
+++ Face/mainPage.h
@@ -0,0 +1,17 @@
+char *mainpage PROGMEM = R"=====(
+<html>
+<head>
+    <title>NSoC Security</title>
+</head>
+<body>
+    <h3>NIE Summer of Code 5.0 | NISB</h3>
+    <p>Authentication Required</p>
+<form action="http://192.168.43.212:8080/val" method="post" enctype="multipart/form-data">
+    <label for="profile_pic">Choose file to upload</label>
+    <input type="file" id="profile_pic" name="profile_pic" size="chars">
+      <input type="submit" name="btn" id="btn" value="Submit">
+</form>
+    <script>
+    </script>
+</body>
+</html>)=====";
