1599e74d8c71ab663c2c50ca6f21a17da38ebf65
shashank-simha <shashank-simha@github.com>
2018-05-26 19:10:57 +0530

    Bluetooth added | SD updated

---
+++ Bluetooth/SerialToSerialBT/SerialToSerialBT.ino
@@ -0,0 +1,40 @@
+#include <DHT.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+BluetoothSerial SerialBT;
+int input = 32;
+DHT dht(input, DHT11); //Initializing the DHT sensor type with this code
+int i,j;
+
+void BT_Print(String x)
+{
+  for(i = 0; i < x.length(); i++)
+  {
+    SerialBT.write(x[i]);
+    delay(20);
+  }
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() 
+{
+  float h = dht.readHumidity(); //Reading Humidity values from DHT11
+  float t = dht.readTemperature(); //Reading Temperature values from DHT11
+  BT_Print("Temparature : ");
+  BT_Print(String(t));
+  BT_Print("\n");
+  BT_Print("Humidity : ");
+  BT_Print(String(h));
+  BT_Print("\n");
+  BT_Print("\n");
+  delay(2000);
+}
--- SD/SD.ino
+++ SD/SD.ino
@@ -57,32 +57,6 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels)
     }
 }
 
-void createDir(fs::FS &fs, const char * path)
-{
-    Serial.printf("Creating Dir: %s\n", path);
-    if(fs.mkdir(path))
-    {
-        Serial.println("Dir created");
-    }
-    else 
-    {
-        Serial.println("mkdir failed");
-    }
-}
-
-void removeDir(fs::FS &fs, const char * path)
-{
-    Serial.printf("Removing Dir: %s\n", path);
-    if(fs.rmdir(path))
-    {
-        Serial.println("Dir removed");
-    }
-    else 
-    {
-        Serial.println("rmdir failed");
-    }
-}
-
 void readFile(fs::FS &fs, const char * path)
 {
     Serial.printf("Reading file: %s\n", path);
@@ -123,40 +97,6 @@ void writeFile(fs::FS &fs, const char * path, const char * message)
     file.close();
 }
 
-void appendFile(fs::FS &fs, const char * path, const char * message)
-{
-    Serial.printf("Appending to file: %s\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file)
-    {
-        Serial.println("Failed to open file for appending");
-        return;
-    }
-    if(file.print(message))
-    {
-        Serial.println("Message appended");
-    }
-    else 
-    {
-        Serial.println("Append failed");
-    }
-    file.close();
-}
-
-void renameFile(fs::FS &fs, const char * path1, const char * path2)
-{
-    Serial.printf("Renaming file %s to %s\n", path1, path2);
-    if (fs.rename(path1, path2)) 
-    {
-        Serial.println("File renamed");
-    }
-    else
-    {
-        Serial.println("Rename failed");
-    }
-}
-
 void deleteFile(fs::FS &fs, const char * path)
 {
     Serial.printf("Deleting file: %s\n", path);
@@ -168,56 +108,6 @@ void deleteFile(fs::FS &fs, const char * path)
     {
         Serial.println("Delete failed");
     }
-}
-
-void testFileIO(fs::FS &fs, const char * path)
-{
-    File file = fs.open(path);
-    static uint8_t buf[512];
-    size_t len = 0;
-    uint32_t start = millis();
-    uint32_t end = start;
-    if(file)
-    {
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        while(len)
-        {
-            size_t toRead = len;
-            if(toRead > 512)
-            {
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            len -= toRead;
-        }
-        end = millis() - start;
-        Serial.printf("%u bytes read for %u ms\n", flen, end);
-        file.close();
-    }
-    else 
-    {
-        Serial.println("Failed to open file for reading");
-    }
-
-
-    file = fs.open(path, FILE_WRITE);
-    if(!file)
-    {
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-
-    size_t i;
-    start = millis();
-    for(i=0; i<2048; i++)
-    {
-        file.write(buf, 512);
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-    file.close();
 }
 
 void setup()
@@ -262,10 +152,10 @@ void setup()
 void loop()
 {
     Serial.println("------------------------------------------------------------------------");
-    Serial.print("Enter 1 to List contents of this directory\nEnter 2 to Create a directory\nEnter 3 to Remove a directory\nEnter 4 to Create a file\nEnter 5 to Read from a file\nEnter 6 to Append to a file\nEnter 7 to Delete a file\nEnter 8 to Rename a file\nEnter 9 to see SDcard memory usage");
+    Serial.print("Enter 1 to List contents of this directory\nEnter 2 to Create a file\nEnter 3 to Read from a file\nEnter 4 to Delete a file\nEnter 5 to see SDcard memory usage\n");
     Serial.println("------------------------------------------------------------------------");
 
-    in = Serial.read();
+    in = 5;//Serial.read();
 
     switch(in)
     {
@@ -273,58 +163,30 @@ void loop()
               break;
       case 2: Serial.println("");
               Serial.println("");
-              Serial.println("Enter the name of the directory");
-              dirname = Serial.read();
-              createDir(SD, (char *)dirname.c_str());
-              break;
-      case 3: Serial.println("");
-              Serial.println("");
-              Serial.println("Enter the name of the directory");
-              dirname = Serial.read();
-              removeDir(SD, (char *)dirname.c_str());
-              break;
-      case 4: Serial.println("");
-              Serial.println("");
               Serial.println("Enter the name of the file");
               filename = Serial.read();
               Serial.println("Enter the content of the file");
               temp = Serial.read();
               writeFile(SD, (char *)filename.c_str(), (char *)temp.c_str());
               break;
-      case 5: Serial.println("");
+      case 3: Serial.println("");
               Serial.println("");
               Serial.println("Enter the name of the file");
               filename = Serial.read();
               readFile(SD, (char *)filename.c_str());
               break;
-      case 6: Serial.println("");
-              Serial.println("");
-              Serial.println("Enter the name of the file");
-              filename = Serial.read();
-              Serial.println("Enter the content to be appended");
-              temp = Serial.read();
-              appendFile(SD, (char *)filename.c_str(), (char *)temp.c_str());
-              break;
-      case 7: Serial.println("");
+      case 4: Serial.println("");
               Serial.println("");
               Serial.println("Enter the name of the file");
               filename = Serial.read();
               deleteFile(SD, (char *)filename.c_str());
               break;
-      case 8: Serial.println("");
-              Serial.println("");
-              Serial.println("Enter the current name of the file");
-              filename = Serial.read();
-              Serial.println("Enter the current name of the file");
-              temp = Serial.read();
-              renameFile(SD, (char *)filename.c_str(), (char *)temp.c_str());
-              break;
-      case 9: Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+      case 5: Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
               Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
               break;
       default:Serial.println("Enter a valid choice");
               break;
     }
-    
+    while(1)
     delay(2000);
 }
