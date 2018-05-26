433eeccd0ffef7e91619758770370cff476ed248
shashank-simha <shashank-simha@github.com>
2018-05-26 22:00:27 +0530

    DHT11 added

---
+++ DHT11/DHT11.ino
@@ -0,0 +1,22 @@
+#include <DHT.h>
+
+#define DHTPIN 0
+
+DHT dht(DHTPIN, DHT11); //Initializing the DHT sensor type with this code
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+}
+
+void loop()
+{
+  float h = dht.readHumidity(); //Reading Humidity values from DHT11
+  float t = dht.readTemperature(); //Reading Temperature values from DHT11
+  Serial.println("Temperature:");
+  Serial.println(t);
+  Serial.print("Humidity:");
+  Serial.print(h);
+  delay(2000);
+}
